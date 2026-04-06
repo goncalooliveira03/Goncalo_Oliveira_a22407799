dcc7bee53067d2c024abf2dd742fa480bf5f4938
goncalooliveira03 <46873718+goncalooliveira03@users.noreply.github.com>
2026-04-06 17:27:53 +0000

    Eliminação de ficheiros não importantes

--- aplicacao_do_quizz/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AplicacaoDoQuizzConfig(AppConfig):
-    name = "aplicacao_do_quizz"
--- aplicacao_do_quizz/models.py
+++
@@ -1,4 +0,0 @@
-from django.db import models
-
-class Aplicacao_do_quizz(models.Model):
-    nome = models.CharField(max_length=100)
